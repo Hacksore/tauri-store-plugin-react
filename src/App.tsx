2fe635a735735ea63619d3ed60e1754246986103
Sean Boult <996134+Hacksore@users.noreply.github.com>
2025-02-10 08:59:24 -0600

    Init

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import "./App.css";
+
+function App() {
+  return (
+    <main className="container">
+      <h1>Testing settings</h1>
+
+      <input type="text" />
+      <input type="checkbox" checked />
+
+    </main>
+  );
+}
+
+export default App;
