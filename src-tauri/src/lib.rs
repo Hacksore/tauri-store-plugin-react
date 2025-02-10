895485b68b44ac6cadc0ef068bf9124d8bbc4c3f
Sean Boult <996134+Hacksore@users.noreply.github.com>
2025-02-10 09:27:34 -0600

    Fixie

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,8 +1,18 @@
+use tauri_plugin_store::StoreExt;
+
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
   tauri::Builder::default()
     .plugin(tauri_plugin_opener::init())
     .plugin(tauri_plugin_store::Builder::default().build())
+    .setup(move |app| {
+
+      let store = app.store("config.json")?;
+
+      println!("Hello, world!, store: {:?}", store.keys());
+
+      Ok(())
+    })
     .run(tauri::generate_context!())
     .expect("error while running tauri application");
 }
