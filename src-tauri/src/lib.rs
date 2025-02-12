9d75ff9de408618cdfcab16d9714bb74cbdcc6fe
Sean Boult <996134+Hacksore@users.noreply.github.com>
2025-02-12 08:51:43 -0600

    Just keep hitting the store it's fine

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -2,23 +2,21 @@ use rand::Rng;
 use tauri::{generate_handler, Manager, WebviewWindow};
 use tauri_plugin_store::StoreExt;
 
+const CONFIG_FILE: &str = "config.json";
+
 #[tauri::command]
 fn test_command(window: WebviewWindow) {
   let app = window.app_handle();
-  // TODO: we should be able to hoist this in the app global state but need to figure that out
-  let store = app.store("config.json").unwrap();
-  let mut rng = rand::thread_rng();
-  let n1: u16 = rng.gen();
+  let store = app.store(CONFIG_FILE).unwrap();
+  let mut rng = rand::rng();
+  let n1: u16 = rng.random();
 
   println!("Setting the state value to something random: {n1}");
 
-  store.set("random_value", n1);
+  store.set("randomNumber", n1);
   _ = store.save();
 }
 
-// struct AppState {
-//   store: Store,
-// }
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
@@ -26,14 +24,9 @@ pub fn run() {
     .plugin(tauri_plugin_opener::init())
     .plugin(tauri_plugin_store::Builder::default().build())
     .setup(move |app| {
-      let store = app.store("config.json")?;
-      // let app_state = AppState { store };
+      let store = app.store(CONFIG_FILE)?;
 
-      // TODO: figure out how to manage the config so we can use it else where like the command
-      // handler above so we don't have more than one instance loaded
-      // app.manage(app_state); // manage this store value in an arc/mutex
-
-      println!("Hello, world!, store: {:?}", store.keys());
+      println!("Store keys: {:?}", store.entries());
 
       Ok(())
     })
